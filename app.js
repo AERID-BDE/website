dd15532a1e59d1d5d3e678b5434f12bdec4f1625
lapizzamolo <lerouxmarin7@gmail.com>
2023-10-18 15:34:24 +0200

    Ajout de la base mongoDB

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 require('dotenv').config();
 
+const mongoose = require('mongoose')
 const express = require('express');
 const path = require('path');
 
 const app = express();
+
+/* --------------------------------- mongoose ------------------------------------- */
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true });
 
 /* --------------------------------- express ------------------------------------- */
 
