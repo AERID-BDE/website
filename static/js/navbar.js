65dd42dffeb1474b3c9791558f8f4124f9d2cac1
William Fontaine <william.fontaine0308@gmail.com>
2023-10-19 16:14:59 +0200

    product list

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -4,11 +4,9 @@ const navbar = document.getElementById("navbar");
 const navItems = navbar.getElementsByClassName("nav-item");
 
 for (let i = 0; i < navItems.length; i++) {
-    let href = navItems[i].getAttribute("href");
+    let href = navItems[i].querySelector("a").getAttribute("href");
 
     if (href === pathname) {
         navItems[i].classList.add("active");
     }
 }
-
-console.log(pathname)
