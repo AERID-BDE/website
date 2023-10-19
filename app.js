9e75e3d6bd682bf9236a6bd1bd8dc6d65e86bfc7
William Fontaine <william.fontaine0308@gmail.com>
2023-10-19 11:52:10 +0200

    require events

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(express.urlencoded({extended: false}));
 require('./src/routes/auth')(app);
 require('./src/routes/home')(app);
 require('./src/routes/product')(app);
-
+require('./src/routes/event')(app);
 
 app.use((req, res) => {
     res.redirect('/');
