da8eeee723f7b208f5c145c8f9acb2cdcbc7cfa4
lapizzamolo <lerouxmarin7@gmail.com>
2023-10-19 11:26:33 +0200

    mise en place des events + newevents

--- src/routes/event.js
+++ src/routes/event.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const EventController = require('../controllers/EventController');
+const AuthController = require('../controllers/AuthController');
+const AuthGuard = require("../guards/AuthGuard");
+
 
 const router = express.Router();
 
 module.exports = (app) => {
     router.get('/', EventController.showAll);
+
+    router.get('/new', EventController.showAddEvent);
+    router.post('/new', EventController.addEvent);
 
     app.use('/events', router);
 }
